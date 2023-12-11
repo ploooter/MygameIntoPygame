826093208af51bd97fb9f7d2a35ad8a6ad01bd88
BL0023r <raxxrajkumarsharma@protonmail.com>
2023-12-11 22:43:06 +0530

    snakeGame init

---
+++ Beginners/SnakeGame.py
@@ -0,0 +1,26 @@
+import pygame
+from pygame.locals import *
+from random import randint
+
+# in game constant
+width, height = 600, 600
+col, row = 30, 30
+snake_speed = 10
+
+class Cube:
+    def __init__(self) -> None:
+        pass
+
+class Snake:
+    def __init__(self) -> None:
+        pass
+
+class Game:
+    def __init__(self) -> None:
+        pass
+    def run(self) -> None:
+        pass
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
